dc48fdf594694a1607c4f8c77913374b6a577674
SAndreeaM <s.andreea001@yahoo.com>
2024-01-19 21:37:27 +0200

    Added info popup and animation; Changed styles

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 var playBtnSufLen = "-play-btn".length;
 var rangeSufLen = "-range".length;
 var volBtnSufLen = "-vol-btn".length;
+var popupSufLen = "-popup".length;
+var infoBtnSufLen = "-info-btn".length;
 
 var lastVolume = 0.5;
 
-function getSound(btnId, btnSufLen) {
-    const sndId = btnId.substring(0, btnId.length - btnSufLen);
-    const snd = document.getElementById(sndId);
-    return {sndId, snd};
-}
-
+// Get Button and ButtonID
 function getButton(event) {
     const btnId = event.target.id;
     const btn = document.getElementById(btnId);
     return {btnId, btn};
 }
 
+// Get Sound and SoundID
+function getSound(btnId, btnSufLen) {
+    const sndId = btnId.substring(0, btnId.length - btnSufLen);
+    const snd = document.getElementById(sndId);
+    return {sndId, snd};
+}
+
+// STOP/PLAY Sound
 function stopPlay(event) {
     const {btnId, btn} = getButton(event);
     const {sndId, snd} = getSound(btnId, playBtnSufLen);
@@ -35,6 +40,7 @@ function stopPlay(event) {
         }
 }
 
+// Modify Sound Volume
 function modifyVolume(event) {
     // TODO: Fix this not working
     //const {rangeId, range} = getButton(event);
@@ -69,6 +75,7 @@ function modifyVolume(event) {
         btn.classList.add("fa-volume-xmark");
 }
 
+// Mute Sound Volume
 function muteVolume(event) {
     btnId = event.target.id;
     btn = document.getElementById(btnId);
@@ -115,4 +122,14 @@ function muteVolume(event) {
                 range.value = lastVolume * 100;
             }
         }
+}
+// Popup Toggle
+function popupToggle(event) {
+    const {btnId, btn} = getButton(event);
+    const {sndId, snd} = getSound(btnId, infoBtnSufLen);
+    const popupId = sndId + "-popup";
+    const popup = document.getElementById(popupId);
+
+    popup.classList.toggle("hidden");
+    popup.classList.toggle("visible");
 }
